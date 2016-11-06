61af0e92a86c8388f94b8301cd1758e2550cf2dd
christian otkjær <christian.otkjaer@gmail.com>
2016-11-06 20:02:37 +0100

    Initial Commit

---
+++ Graph/Graph.h
@@ -0,0 +1,19 @@
+//
+//  Graph.h
+//  Graph
+//
+//  Created by Christian Otkjær on 06/11/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graph.
+FOUNDATION_EXPORT double GraphVersionNumber;
+
+//! Project version string for Graph.
+FOUNDATION_EXPORT const unsigned char GraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graph/PublicHeader.h>
+
+
